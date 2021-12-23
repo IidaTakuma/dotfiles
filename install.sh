463d6053223baefbed7f08852b15e37164b8f197
IidaTakuma <ta9maxxx@gmail.com>
2021-12-23 23:25:07 +0900

    add install.sh & edit .gitconfig

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -u
+
+# 実行中のシェルスクリプトのあるディレクトリに移動
+BASE_DIR=$(dirname $0)
+cd $BASE_DIR
+
+for f in .??*; do # ワイルドカード / .?[任意の1文字]?[任意の1文字]*[任意の0文字以上の文字列]
+    [ "$f" = ".git" ] && continue
+    [ "$f" = ".DS_Store" ] && continue
+    ln -snfv ${PWD}/"$f" ~
+done
