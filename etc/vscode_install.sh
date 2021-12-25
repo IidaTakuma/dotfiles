c16b0d7f31fd1db370865ef320c95bee0b0579d2
IidaTakuma <ta9maxxx@gmail.com>
2021-12-25 19:06:53 +0900

    os初期化から退避

--- etc/vscode_install.sh
+++ etc/vscode_install.sh
@@ -1,17 +1,25 @@
 #!/bin/sh
 
-SCRIPT_DIR=$(cd $(dirname $0) && pwd)
-VSCODE_SETTING_DIR=~/Library/Application\ Support/Code/User
+set -u
+
+VSCODE_SETTING_DIR="${HOME}/Library/Application Support/Code/User"
+
+# 実行中のシェルスクリプトのあるディレクトリを起点に .vscode ディレクトリに移動
+SCRIPT_DIR=$(cd $(dirname $0)/../.vscode && pwd)
+echo $SCRIPT_DIR
 
 rm "$VSCODE_SETTING_DIR/settings.json"
-ln -s "$SCRIPT_DIR/settings.json" "${VSCODE_SETTING_DIR}/settings.json"
+ln -s "${SCRIPT_DIR}/settings.json" "${VSCODE_SETTING_DIR}/settings.json"
 
 rm "$VSCODE_SETTING_DIR/keybindings.json"
-ln -s "$SCRIPT_DIR/keybindings.json" "${VSCODE_SETTING_DIR}/keybindings.json"
+ln -s "${SCRIPT_DIR}/keybindings.json" "${VSCODE_SETTING_DIR}/keybindings.json"
 
-cat ./extensions | while read line
+cd $SCRIPT_DIR
+
+cat extensions | while read line
 do
- code --install-extension $line
+    echo $line
+    code --install-extension $line
 done
 
 code --list-extensions > extensions
